ac318cfd8e22d03d41e239f08b618ba19bf58491
Administrator <admin@example.com>
2021-04-22 14:06:32 +0430

    removed grpcio-tools

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # Needed to actually package something
     packages=['ai_connector','ai_connector/client', 'ai_connector/utils'],
     # Needed for dependencies
-    install_requires=['grpcio', 'grpcio-tools'],
+    install_requires=['grpcio'],
     # *strongly* suggested for sharing
     version='0.1',
 )
