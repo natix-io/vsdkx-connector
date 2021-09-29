36ba74cc33330491d8df31099f02cba25479475d
gja <g.mekokishvili@omedia.dev>
2021-09-29 11:38:41 +0400

    change to github

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='AI Connector',
-    url='https://gitlab.com/natix/visiondeploy/aiconnector',
+    name='vsdkx-connector',
+    url='https://github.com/natix-io/vsdkx-connector.git',
     author='Guja',
-    author_email='guja@natix.io',
+    author_email='g.mekokishvili@omedia.ge',
     # Needed to actually package something
     packages=['ai_connector','ai_connector/client', 'ai_connector/utils'],
     # Needed for dependencies
